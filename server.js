5a85b36066c4e0a2db88803859a46ea6e5d91b5f
Brian Stovall <hoppenmaus@gmail.com>
2016-02-13 08:49:54 -0500

    started on url database code

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var path = require('path');
+var mongo = require('mongodb').MongoClient;
 
 var app = express();
 
+mongo.connect ('mongodb://localhost:27017/urlShortener', function (err, db) {
+	var collection = db.collection('urls');
+	//collection.insert({'original_url': 'http://www.xkcd.com', 
+										 //'short_url': 'https://shorty.herokuapp.com/2'});
+collection.find().toArray( (err, docs) => {
+	console.log(docs.length + ' length')
+	for (var i = 0; i < docs.length; i ++)
+		console.log(docs[i]);
+	db.close();
+});
+
+});
+	
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/new/:data', (request, response) => {
