c7c304e55e3a725ad336fede521418c850570da1
Brian Stovall <hoppenmaus@gmail.com>
2016-02-13 21:45:39 -0500

    started regexp test for valid urls

--- server.js
+++ server.js
@@ -4,32 +4,32 @@ var mongo = require('mongodb').MongoClient;
 
 var app = express();
 
+/*
 mongo.connect ('mongodb://localhost:27017/urlShortener', function (err, db) {
 	var collection = db.collection('urls');
 	//collection.insert({'original_url': 'http://www.xkcd.com', 
 										 //'short_url': 'https://shorty.herokuapp.com/2'});
-collection.find().toArray( (err, docs) => {
-	console.log(docs.length + ' length')
-	for (var i = 0; i < docs.length; i ++)
-		console.log(docs[i]);
-	db.close();
+	collection.find().toArray( (err, docs) => {
+		console.log(docs.length + ' length')
+		for (var i = 0; i < docs.length; i ++)
+			console.log(docs[i]);
+		db.close();
+	});
 });
-
-});
+*/
 	
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/new/:data', (request, response) => {
-	//set up response header
+app.get(/(^http?s*\.*)/, (request, response) => {
+	var returnValue = request.params.toString();
+	
+	//set up response header and send data
 	response.setHeader('Content-Type', 'application/json');
-
-	response.write(JSON.stringify({'unix'   : null,
-																 'natural': null}),
-								 (err) => {response.send();});
+	response.write(returnValue, (err) => {response.send();});
 });
 
 app.use( (request, response) => {
-		response.sendFile(__dirname + '/public/index.html');
+	response.write('error: invalid url', (err) => {response.send();});
 });
 
 
