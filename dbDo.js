c55b9919630fc023401aa226d3210bb9bfb6043b
Brian Stovall <hoppenmaus@gmail.com>
2016-02-14 14:29:54 -0500

    moved db functions into dbDo.js. build /new/ function, working on short url retrieval

---
+++ dbDo.js
@@ -0,0 +1,44 @@
+//in the "urls" collection, the _id field is the original url
+//and the short_url field is a unique number associated with the url
+
+var mongo = require('mongodb').MongoClient;
+var appDir = 'https://shorty.herokuapp.com/';
+var connectData = 'mongodb://localhost:27017/urlShortener';
+
+//accepts a number and returns a url if one exists, or false if not
+exports.fetch = function(num, callback) {
+	return mongo.connect ('mongodb://localhost:27017/urlShortener', function (err, db) {
+		var retval;
+		var collection = db.collection('urls');
+		collection.find({'short_url': num}).toArray( (err, docs) => {
+			if (docs.length) retval = JSON.strigify(docs[0]._id);
+			else retval = 'false';
+			callback(retval);
+			db.close();
+		});
+	});
+};
+
+//makes a new entry for a url if it doesn't already exist
+exports.make = function(url, callback) {
+	mongo.connect ('mongodb://localhost:27017/urlShortener', function (err, db) {
+		var collection = db.collection('urls');
+		var retval;
+		return collection.count( {}, (err, count) => {
+			return collection.find({'_id': url}).toArray( (err, docs) => {
+				//console.log('in make -> tried to find matching url, found :' +  docs.length);
+				if (!docs.length) {
+					//get the number of items in the collection and use it to 
+					//make the short_url
+						var doc = {'_id': url,
+											 'short_url':appDir + count};
+						collection.insert(doc);
+						retval = doc;
+				} else retval = {'error' : 'duplicate url'};
+				db.close();
+				//console.log('fn make in dbDo is returning ' + JSON.stringify(retval));
+				callback(retval);
+			});
+		});
+	});
+};
--- server.js
+++ server.js
@@ -2,46 +2,49 @@ var express = require('express');
 var path = require('path');
 var mongo = require('mongodb').MongoClient;
 
+//a module that provides the commands 'make' and 'fetch' 
+var dbDo = require('./dbDo');
+
+//the url of the service
+var appDir = 'https://shorty.herokuapp.com/';
+
 var app = express();
 
-/*
-mongo.connect ('mongodb://localhost:27017/urlShortener', function (err, db) {
-	var collection = db.collection('urls');
-	//collection.insert({'original_url': 'http://www.xkcd.com', 
-										 //'short_url': 'https://shorty.herokuapp.com/2'});
-	collection.find().toArray( (err, docs) => {
-		console.log(docs.length + ' length')
-		for (var i = 0; i < docs.length; i ++)
-			console.log(docs[i]);
-		db.close();
-	});
-});
-*/
+//helper to serve as callback to dbDo methods
+function writeJSON (stream, data) {
+	stream.setHeader('Content-Type', 'application/json');
+	stream.write( JSON.stringify(data),
+			(err) => {if (err) console.log(err); stream.send()});
+}
+
+function writeString (stream, data) {
+	stream.setHeader('Content-Type', 'application/json');
+	stream.write( data,
+			(err) => {if (err) console.log(err); stream.send()});
+}
 	
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 //handle calls to /new with good urls (per spec: http(s):// at least one char
 //followed by at least one dot, followed by at least one char
 app.get(/\/new\/(http[s]?:\/\/.+\..+)/, (request, response) => {
-	var returnValue = request.params[0].toString();
-	
-	//set up response header and send data
-	response.setHeader('Content-Type', 'application/json');
-	response.write(JSON.stringify({'original_url': returnValue,
-		                             'short_url': null }), (err) => {response.send();});
+	var newWrite = writeJSON.bind(null, response);
+	dbDo.make(request.params[0].toString(), newWrite);
 });
 
 //handle calls to /new with bad urls
 app.get(/\/new\/(?!http[s]?:\/\/.+\..+)/, (request, response) => {
 	response.setHeader('Content-Type', 'application/json');
-	response.write(JSON.stringify({'error': 'url invalid'}), (err) => {response.send();});
+	response.write(JSON.stringify({'error': 'url invalid'}),
+		(err) => {if (err) console.log(err); response.send()});
 });
+
 
 //handle calls to /list by displaying the db contents
 app.get('/list', (request, response) => {
 	mongo.connect ('mongodb://localhost:27017/urlShortener', function (err, db) {
 		var collection = db.collection('urls');
-		collection.find({}, {'original_url':1, 'short_url':1, _id:0}).
+		collection.find({}, {'original_url':1, 'short_url':1, _id:1}).
 			toArray( (err, docs) => {
 			response.setHeader('Content-Type', 'application/json');
 			for (var i = 0; i < docs.length; i++)
@@ -50,6 +53,12 @@ app.get('/list', (request, response) => {
 			db.close();
 		});
 	});
+});
+
+//handle shortened url requests
+app.get('/:num', (request, response) => {
+	var shortWrite = writeString.bind(null, response);
+	dbDo.fetch(request.params.num, shortWrite);
 });
 
 //catch-all for bad addresses that redirects to instruction page
